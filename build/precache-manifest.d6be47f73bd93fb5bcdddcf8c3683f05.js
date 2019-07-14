e6c60c3363df4a6089636a53cbdf6ed42625c2a5
Niko Hienonen <niko.hienonen@gmail.com>
2019-07-14 17:46:55 +0300

    netlify

---
+++ build/precache-manifest.d6be47f73bd93fb5bcdddcf8c3683f05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0a28a9de76bad1be16d3e04c388a1e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df50e55f1fb0a1ed9c08",
+    "url": "/static/css/main.362fbf28.chunk.css"
+  },
+  {
+    "revision": "c8cc106333ad32826a77",
+    "url": "/static/js/2.74b707e1.chunk.js"
+  },
+  {
+    "revision": "df50e55f1fb0a1ed9c08",
+    "url": "/static/js/main.ff955c1d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
